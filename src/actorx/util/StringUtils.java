7c09cb4a6ebf81dfeedd1929107999ddeab5fdcf
xiongxiaolei <34894419@qq.com>
2017-02-16 18:30:29 +0800

    add StringUtils

---
+++ src/actorx/util/StringUtils.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package actorx.util;
+
+/**
+ * @author Xiong
+ * @creation 2017年2月16日下午6:23:03
+ *
+ */
+public final class StringUtils {
+	
+	/**
+	 * 是否为空
+	 * @param str
+	 * @return
+	 */
+	public static boolean isEmpty(String str){
+		return str == null || str.isEmpty();
+	}
+	
+}
